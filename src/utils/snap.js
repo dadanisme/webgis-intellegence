22b37136b5caf6b5e8024a3c73b6542937835c74
Ramdan <91539532+dadanisme@users.noreply.github.com>
2022-12-10 09:07:24 +0700

    Update client key

--- src/utils/snap.js
+++ src/utils/snap.js
@@ -2,7 +2,7 @@ export const getSnap = () => {
   const script = document.createElement("script");
   script.src = "https://app.sandbox.midtrans.com/snap/snap.js";
   script.type = "text/javascript";
-  script.setAttribute("data-client-key", "SB-Mid-client-EKj8tcMtOvAqB6Lt");
+  script.setAttribute("data-client-key", "SB-Mid-client-84PD1yfgn32Vwa-z");
 
   // insert the script tag to head
   document.head.appendChild(script);
